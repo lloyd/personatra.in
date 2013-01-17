764bf20170ab19edc1f7742d9630e7895b9e9941
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-01-16 21:24:50 -0700

    meh

---
+++ js/cal.js
@@ -0,0 +1,6 @@
+var Calendar = {
+  render: function(selector) {
+    $(selector).empty();
+    $(selector).text("hi mom");
+  }
+};
---
+++ js/main.js
@@ -0,0 +1,3 @@
+$(function($){
+  Calendar.render(".calendar");
+});
