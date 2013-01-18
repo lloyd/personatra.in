960a3f2aca16f6d48e3a926de5ef42f7cebce02c
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-01-17 21:15:02 -0700

    some fonts and get current train calculation working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 function whatTrain() {
-  return "2013.01.18";
+  var ref = "2013.01.04";
+  // calculate the "current" train from a reference train
+  var days = moment(ref)
+    .subtract("days", 2)
+    // this is a thursday, right after one train ships (wed)
+    // and before the next is cut (friday)
+    .diff(moment(), 'days');
+
+  return moment(ref).add('days', Math.floor(-days / 14) * 14).format("YYYY.MM.DD");
 }
 
 $(function($){
   // determine what train we're talking about
   var train = whatTrain();
   $(".train .name").text(train);
-  $(".train .branches").text(moment(train).fromNow());
+  $(".train .branches").text(moment(train).eod().fromNow());
   $(".train .ships").text(moment(train).add("days", 12).fromNow());
   Calendar.render(".calendar", train);
 });
