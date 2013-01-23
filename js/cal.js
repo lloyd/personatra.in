84d1fbcec563736bc554d0ec86f1fe9d26094cb8
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-01-22 17:20:18 -0700

    a very bare MVP

--- js/cal.js
+++ js/cal.js
@@ -1,7 +1,33 @@
 var Calendar = {
   // render the calendar given the specified train
   render: function(selector, train) {
-    train = train || '2012.01.11';
-    console.log(moment(train).fromNow());
+
+    // first update the visual calendar
+    var m = moment(train).subtract('days', 12);
+    var kids = $(".calendar > div");
+    for (var i = 0; i < kids.length; i++) {
+      $(kids[i]).text(m.format("MM/DD"));
+      if (i > 10 && i < 25) $(kids[i]).addClass("train");
+      if (m.sod().diff(moment().sod()) === 0) $(kids[i]).addClass("today");;
+      m.add('days', 1);
+    }
+
+    // next populate key dates.
+    var keyDates = $("div.date");
+    for (var i = 0; i < keyDates.length; i++) {
+      var x = parseInt($(keyDates[i]).attr("offset"));
+      $(keyDates[i]).find(".when").text(moment(train).add('days', x).format("MM/DD"));
+    }
+
+    $("div.date").on('mouseover', function() {
+      var x = $("div.calendar > div")[12 + parseInt($(this).attr('offset'))];
+      // which cell
+      $(x).addClass("highlighted");
+    });
+    $("div.date").on('mouseout', function() {
+      var x = $("div.calendar > div")[12 + parseInt($(this).attr('offset'))];
+      // which cell
+      $(x).removeClass("highlighted");
+    });
   }
 };
